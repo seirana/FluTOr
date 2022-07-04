fbab9fd03e4b6a699bd32fd68d4c61ac7cbec535
Seirana Hashemi <seirana.hashemi@gmail.com>
2022-07-04 14:34:14 +0200

    Update Trade_offs_athaliana.m

--- Trade_offs_athaliana.m
+++ Trade_offs_athaliana.m
@@ -1,13 +1,13 @@
 clc;clear;
-
+% read A. thaliana model and find trade-offs
 file_name = 'ArabidopsisCoreModel';
 rnd =1e-5;  % flux accuracy equals 1e-5
-nit_amu = {'NO3','NH4','50:50'};
-active_bio = {'Bio_CLim','Bio_NLim','Bio_opt'};
+nit_amu = {'NO3','NH4','50:50'}; % nitrogen source
+active_bio = {'Bio_CLim','Bio_NLim','Bio_opt'}; % active biomass 
 
-for b = 1:3
-    for na = 1:3
-        for j = 1:3
+for b = 1:3 % loop for active biomass
+    for na = 1:3 % loop for nictogen source
+        for j = 1:3 % loop for value of biomass lower bound
             lb_bio = 0.85+j*0.05;
             
             [mdl1, biomass] = model_athaliana(file_name, rnd, lb_bio, active_bio(b), nit_amu(na)); % read the model
