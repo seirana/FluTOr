06a0817729937f96fecfeb2ae5bff026c0da8976
Seirana Hashemi <seirana.hashemi@gmail.com>
2022-07-04 14:17:32 +0200

    Update Trade_offs_athaliana.m

--- Trade_offs_athaliana.m
+++ Trade_offs_athaliana.m
@@ -1,7 +1,7 @@
 clc;clear;
 
 file_name = 'ArabidopsisCoreModel';
-rnd =1e-5;
+rnd =1e-5;  % flux accuracy equals 1e-5
 nit_amu = {'NO3','NH4','50:50'};
 active_bio = {'Bio_CLim','Bio_NLim','Bio_opt'};
 
@@ -12,9 +12,9 @@ for b = 1:3
             
             [mdl1, biomass] = model_athaliana(file_name, rnd, lb_bio, active_bio(b), nit_amu(na)); % read the model
             
-            mdl2 = QFCA(mddl1, rnd); % modified QFCA functions
-            mdl2.F2C2.fctable = FC_F2C2(mdl2);
-            model = QFCA_F2C2_subscription(mdl2);
+            mdl2 = QFCA(mddl1, rnd); % modified QFCA function
+            mdl2.F2C2.fctable = FC_F2C2(mdl2); % modified F2C2 function
+            model = QFCA_F2C2_subscription(mdl2); % find mutual fully coupled reactions
             
             f_name = strcat(file_name, '_bio=', string(active_bio(b)), '_', string(nit_amu(na)), '_lb=', string(lb_bio));
             save(strcat(adrs, f_name, '_model.mat'),'model'); % save model
