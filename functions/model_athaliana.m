3aaa491ebd1fe0e8c3ccb55b2873f166999eed1f
Seirana Hashemi <seirana.hashemi@gmail.com>
2022-07-18 22:06:22 +0200

    Update model_athaliana.m

--- functions/model_athaliana.m
+++ functions/model_athaliana.m
@@ -1,5 +1,21 @@
 function [model, biomass] = model_athaliana(file_name, rnd, lb_bio, bio, nit_amu)
-%%% flux in entire cone
+% model_athaliana2 reads the original model and modifies based on the conditions of the problem
+%
+% USAGE:
+%     [model, biomass] = model_athaliana(file_name, rnd, lb_bio, bio, nit_amu)
+%
+% INPUTS:
+%     file_name: the name of the original model to load
+%     rnd: flux accuracy (default: 1e-5)
+%
+% OUTPUT:
+%     model: the metabolic network with fields:
+%         * .QFCA.fctable - a matrix that shows reaction between each two reactions defined by QFCA method
+%         * .F2C2.fctable - a matrix that shows reaction between each two reactions defined by F2C2 method
+%         * .QFCA.FCref - returns reference number for fullycpoupled reactions
+%         * .QFCA.rxns - the list reaction names which are fully coupled
+%         * .QFCA.rxnN - the list reaction numbers which are fully coupled
+%         * .QFCA.FC- the list reaction names which are fully coupled
 
 load(strcat(file_name,'.mat')); % load the model
 
