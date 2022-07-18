f2f6da01714a3390fe3b4e15c48e9fb1a3b59cc4
Seirana Hashemi <seirana.hashemi@gmail.com>
2022-07-18 19:00:20 +0200

    Update INFO.m

--- functions/INFO.m
+++ functions/INFO.m
@@ -1,5 +1,25 @@
-function [rxns, model, irr_lst, fix_lst, bio_lst, bio_QFCA, bio] = INFO(model, biomass)
-% this function lists fixed and variable reactions
+function [rxns, irr_lst, fix_lst, bio_lst, bio_QFCA, bio] = INFO(model, biomass)
+% INFO reads the model and prepars the list of fixed and irreversible reactions
+%
+% USAGE:
+%     [rxns, irr_lst, fix_lst, bio_lst, bio_QFCA, bio] = INFO(model, biomass)
+%
+% INPUTS:
+%     model: the metabolic network with fields:
+%         * .QFCA.rxns - the cell array of list of fully coupled reactions
+%         * .rxns - the cell array of reaction abbreviations
+%         * .QFCA.FC - a table that shows each two reactions are fully coupled or not
+%         * .lb -   the doulbe array of reaction flux lower bound
+%         * .ub -   the doulbe array of reaction flux upper bound
+%     biomass: the name of biomass reaction
+%
+% OUTPUT:
+%     rxns: a cell array that the first column refers to reaction numbers adn the second column refers to reaction names 
+%     irr_lst: a list of the irreversible reactions with variable flux
+%     fix_lst: a list of the reactions with  a fixed fluxv value
+%     bio_lst: the list of reactions that fully coupled to biomass reaction
+%     bio_QFCA: the index that shows this column of model.QFCA.rxns belng to biomass reaction and its fully couples reactions
+%     bio: the number of the reaction 
 
 bio_QFCA = strfind(string(model.QFCA.rxns),biomass);
 if iscell(bio_QFCA)
