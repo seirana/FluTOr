4d012fafe6e8d85c640306ef19677014b5c66a99
Seirana Hashemi <seirana.hashemi@gmail.com>
2022-07-18 22:48:33 +0200

    Update rTOrnd.m

--- functions/rTOrnd.m
+++ functions/rTOrnd.m
@@ -1,4 +1,15 @@
 function r = rTOrnd(rnd) 
+% rTOrnd read flux accuracy value(rnd) and return the number of digits after decimal point
+%
+% USAGE:
+%     r = rTOrnd(rnd) 
+%
+% INPUTS:
+%     rnd: the flux accuracy
+%
+% OUTPUT:
+%     r: the number of digits after zero
+
 r = 0;
 while rnd < 1
     rnd = rnd*10;
