1f2e81737132b3676e52073b70f10f628fa68357
Seirana Hashemi <seirana.hashemi@gmail.com>
2022-07-18 22:21:08 +0200

    Update model_ecoliijo1366_davidi.m

--- functions/model_ecoliijo1366_davidi.m
+++ functions/model_ecoliijo1366_davidi.m
@@ -1,4 +1,24 @@
 function [model, biomass] = model_ecoliijo1366_davidi(file_name, rnd, condition)
+% model_ecoliijo1366_davidi reads the original model and modifies based on the conditions of the problem
+%
+% USAGE:
+%     [model, biomass] = model_ecoliijo1366_davidi(file_name, rnd, condition)
+%
+% INPUTS:
+%     file_name: the name of the original model to load
+%     rnd: flux accuracy (default: 1e-5)
+%     condition: the number of the sub-model
+
+
+% OUTPUT:
+%     model: the metabolic network with fields:
+%         * .lb - the lower bound of reaction fluxes
+%         * .ub - the upper bound of reaction fluxes
+%         * .rxns - reaction names
+%         * .mets - metabolite names
+%         * .S - stoichiometric matrix
+%         * .c - objective coefficients
+%     biomass: the name of active biomass reaction
 
 load(strcat(file_name,'.mat')); % load the model
 carbon_source = xlsread(strcat(file_name, '_CarbonSources.xlsx'));
