4b30f046889f7da984e19cbacf545cf2e0f4b0c6
Seirana Hashemi <seirana.hashemi@gmail.com>
2022-07-18 22:18:16 +0200

    Update model_ecoliijo1366_carbonsource.m

--- functions/model_ecoliijo1366_carbonsource.m
+++ functions/model_ecoliijo1366_carbonsource.m
@@ -1,4 +1,25 @@
 function [model, biomass] = model_ecoliijo1366_carbonsource(file_name, rnd, lb_bio, carbon_source, c)
+%model_ecoliijo1366_carbonsource reads the original model and modifies based on the conditions of the problem
+%
+% USAGE:
+%     [model, biomass] = model_ecoliijo1366_carbonsource(file_name, rnd, lb_bio, carbon_source, c)
+%
+% INPUTS:
+%     file_name: the name of the original model to load
+%     rnd: flux accuracy (default: 1e-5)
+%     lb_bio: the rio of lower bound of lower bound of biomass to the optimum biomass flux value
+%     carbon_source: the list of carbon sources
+%     c: the active carbon source
+
+% OUTPUT:
+%     model: the metabolic network with fields:
+%         * .lb - the lower bound of reaction fluxes
+%         * .ub - the upper bound of reaction fluxes
+%         * .rxns - reaction names
+%         * .mets - metabolite names
+%         * .S - stoichiometric matrix
+%         * .c - objective coefficients
+%     biomass: the name of active biomass reaction
 
 load(strcat(file_name,'.mat')); % load the model
 
