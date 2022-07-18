ad5fbb8c0e5dfe95b91de8c9a88a6ebbeac582e9
Seirana Hashemi <seirana.hashemi@gmail.com>
2022-07-18 22:15:31 +0200

    Update model_athaliana.m

--- functions/model_athaliana.m
+++ functions/model_athaliana.m
@@ -7,15 +7,18 @@ function [model, biomass] = model_athaliana(file_name, rnd, lb_bio, bio, nit_amu
 % INPUTS:
 %     file_name: the name of the original model to load
 %     rnd: flux accuracy (default: 1e-5)
-%
+%     lb_bio: the rio of lower bound of lower bound of biomass to the optimum biomass flux value
+%     bio: the name of active biomass raction
+%     nit_amu: the uptake ratio of nitrate to ammunium
+
 % OUTPUT:
 %     model: the metabolic network with fields:
-%         * .QFCA.fctable - a matrix that shows reaction between each two reactions defined by QFCA method
-%         * .F2C2.fctable - a matrix that shows reaction between each two reactions defined by F2C2 method
-%         * .QFCA.FCref - returns reference number for fullycpoupled reactions
-%         * .QFCA.rxns - the list reaction names which are fully coupled
-%         * .QFCA.rxnN - the list reaction numbers which are fully coupled
-%         * .QFCA.FC- the list reaction names which are fully coupled
+%         * .lb - the lower bound of reaction fluxes
+%         * .ub - the upper bound of reaction fluxes
+%         * .rxns - reaction names
+%         * .mets - metabolite names
+%         * .S - stoichiometric matrix
+%         * .c - objective coefficients
 
 load(strcat(file_name,'.mat')); % load the model
 
