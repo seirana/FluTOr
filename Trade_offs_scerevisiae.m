d28c55d8d51f70b4ecf816c90676eebce8510990
Seirana Hashemi <seirana.hashemi@gmail.com>
2022-07-19 15:24:47 +0200

    Update Trade_offs_scerevisiae.m

--- Trade_offs_scerevisiae.m
+++ Trade_offs_scerevisiae.m
@@ -9,17 +9,9 @@ cb = string(cb(2:end,3));
 % find trade-offs for each active carbon source
 for c = 1:size(carbon_source,1)  % loop for active carbon source
     for j = 1:3 % loop for biomass lower bound
-        lb_bio = 0.85+j*0.05; % biomass lower bound
-        
-        [mdl1, biomass] = model_scerevisiae_carbonsource(adr, file_name, rnd, lb_bio, carbon_source, c); % read the model
-        
-        mdl2 = QFCA(mdl1, rnd); % modified QFCA functions
-        mdl2.F2C2.fctable = FC_F2C2(mdl2); % modified F2C2 function
-        model = QFCA_F2C2_subscription(mdl2); % find mutual fully coupled reactions
-        
-        f_name = strcat(file_name, '_CB=', string(cb(c,1)), '_bioLB=', string(lb_bio));
-        save(strcat(adrs, f_name, '_model.mat'),'model'); % save model
-        
+        lb_bio = 0.85+j*0.05; % biomass lower bound        
+        [model, biomass] = model_scerevisiae_carbonsource(adr, file_name, rnd, lb_bio, carbon_source, c); % read the model
+        model = CPR(model, rnd, file_name)  % find coupling relation between each pair of reactions          
         tradeoff_seaker(adrs, f_name, model, biomass); % find_tradeoffs
     end
 end
